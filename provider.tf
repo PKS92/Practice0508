be3c302606fa9f24ecbdadf0617a5f4f9d01929d
PKS92 <kumarp.shukla@gmail.com>
2025-08-31 18:34:38 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -5,6 +5,14 @@ terraform {
             version ="4.29.0"
         }
     }
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "june25"
+    storage_account_name = "june25sa"
+    container_name       = "june25container"
+    key                  = "RYAN.tfstate"
+  }
+}
 }
 provider "azurerm" {
     features {}
